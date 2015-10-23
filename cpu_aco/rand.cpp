113c31bc3d3dd814057a14aac4b52a7819a23437
bolan2014 <1139217488@qq.com>
2015-10-23 11:09:38 +0800

    update function

--- cpu_aco/rand.cpp
+++ cpu_aco/rand.cpp
@@ -2,7 +2,13 @@
 
 #include "rand.h"
 
-int rnd(int nLow,int nUpper)
+int rnd(int nLow, int nUpper)
 {
-    return (int)(nLow+(nUpper-nLow)*rand()/((double)RAND_MAX+1.0));
+    return (int)(nLow+(nUpper-nLow)*rand() / ((double)RAND_MAX+1.0));
+}
+
+double rnd(double dbLow,double dbUpper)
+{
+    double dbTemp=rand() / ((double)RAND_MAX+1.0);
+    return dbLow + dbTemp*(dbUpper-dbLow);
 }
