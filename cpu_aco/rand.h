0c5cf32c7365bcc5b275f2b5f3387bb20f906e6a
bolan2014 <1139217488@qq.com>
2015-10-23 12:30:18 +0800

    update random func

--- cpu_aco/rand.h
+++ cpu_aco/rand.h
@@ -1,3 +1,9 @@
+/* generate random numbers for ants to choose cities */
+
 #include <stdlib.h>
 
+//return random integer between Low and Upper
 int rnd(int nLow,int nUpper);
+
+//return random double between Low and Upper
+double rnd(double dbLow,double dbUpper);
