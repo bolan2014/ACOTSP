18f6210fe455341057f35608050337e216bf4b59
bolan2014 <1139217488@qq.com>
2015-10-23 12:50:56 +0800

    add ants'info

--- cpu_aco/ant.h
+++ cpu_aco/ant.h
@@ -2,25 +2,25 @@
 
 class CAnt
 {
-public:
+  public:
     CAnt(void);
     ~CAnt(void);
 
-public:
+  public:
 
-    int m_nPath[N_CITY_COUNT]; //蚂蚁走的路径
-    double m_dbPathLength; //蚂蚁走过的路径长度
+    int m_nPath[N_CITY_COUNT]; //tour of an ant
+    double m_dbPathLength; //tour length
 
-    int m_nAllowedCity[N_CITY_COUNT]; //没去过的城市
-    int m_nCurCityNo; //当前所在城市编号
-    int m_nMovedCityCount; //已经去过的城市数量
+    int m_nAllowedCity[N_CITY_COUNT]; //cities have not been visited
+    int m_nCurCityNo; //id of the current city
+    int m_nMovedCityCount; //number if cities already visited
 
-public:
+  public:
 
-    int ChooseNextCity(); //选择下一个城市
-    void Init(); //初始化
-    void Move(); //蚂蚁在城市间移动
-    void Search(); //搜索路径
-    void CalPathLength(); //计算蚂蚁走过的路径长度
+    int ChooseNextCity(); //ants choose next city
+    void Init(); //initial ants'info
+    void Move(); //ants'move
+    void Search(); //ants search path
+    void CalPathLength(); //calculate length of path
 
 };
