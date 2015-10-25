ee37cc1c586ec269741a832b94c1095d39ca0a2a
bolan2014 <1139217488@qq.com>
2015-10-25 19:48:03 +0800

    movo out ant structs and funcs

--- gpu_aco/acotsp.cu
+++ gpu_aco/acotsp.cu
@@ -2,15 +2,18 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
-#include <curand.h> //for parallel cuRand
+#include <curand.h>
 
-//const double ALPHA=2.0; //启发因子，信息素的重要程度
-//const double BETA=3.0;   //期望因子，城市间距离的重要程度
+#include "rand.h"
+#include "ant.h"
+
+//const double ALPHA=2.0; //Æ·¢Ò×£¬ÐϢËµÄØª³̶È/
+//const double BETA=3.0;
 const double ROU=0.5; //信息素残留参数
 
-const int N_ANT_COUNT=256; //蚂蚁数量
+//const int N_ANT_COUNT=256; //蚂蚁数量
 const int N_IT_COUNT=15; //迭代次数
-const int N_CITY_COUNT=51; //城市数量
+//const int N_CITY_COUNT=51; //城市数量
 
 const double DBQ=100.0; //总的信息素
 const double DB_MAX=10e9; //一个标志数，10的9次方
@@ -45,176 +48,6 @@ void dev_rnd(unsigned int nSeed)
 double ROUND(double dbA)
 {
     return (double)((int)(dbA+0.5));
-}
-
-//定义蚂蚁类
-class CAnt
-{
-public:
-    CAnt(void);
-    ~CAnt(void);
-
-public:
-
-    int m_nPath[N_CITY_COUNT]; //蚂蚁走的路径
-    double m_dbPathLength; //蚂蚁走过的路径长度
-
-    int m_nAllowedCity[N_CITY_COUNT]; //没去过的城市
-    int m_nCurCityNo; //当前所在城市编号
-    int m_nMovedCityCount; //已经去过的城市数量
-
-public:
-
-    __device__ void antInit(int antID,float *devData);  //初始化
-    __device__ void antMove(int antID,double *d_Distance,double *d_Trial,float *devData); //蚂蚁在城市间移动
-    __device__ void antCalPathLength(int antID,double *d_Distance);  //计算蚂蚁走过的路径长度
-    __device__ int antChooseNextCity(int antID,int count,double *d_Distance,double *d_Trial,float *devData);
-};
-
-//构造函数
-CAnt::CAnt(void)
-{
-}
-
-//析构函数
-CAnt::~CAnt(void)
-{
-}
-
-//////////////////////////////////////////////////////////
-//device or kernel functions
-
-//初始化函数，蚂蚁搜索前调用
-__device__
-void CAnt:: antInit(int antID,float *devData)
-{
-    for (int i=0;i<N_CITY_COUNT;i++)
-    {
-        m_nAllowedCity[i]=1; //设置全部城市为没有去过
-        m_nPath[i]=0; //蚂蚁走的路径全部设置为0
-    }
-
-    //蚂蚁走过的路径长度设置为0
-    m_dbPathLength=0.0;
-
-    //随机选择一个出发城市
-    m_nCurCityNo=(int)N_CITY_COUNT*devData[antID];
-
-    //把出发城市保存入路径数组中
-    m_nPath[0]=m_nCurCityNo;
-
-    //标识出发城市为已经去过了
-    m_nAllowedCity[m_nCurCityNo]=0;
-
-    //已经去过的城市数量设置为1
-    m_nMovedCityCount=1;
-}
-
-//选择下一个城市
-//返回值 为城市编号
-__device__
-int CAnt::antChooseNextCity(int antID,int count,double *d_Distance,double *d_Trial,float *devData)
-{
-    int nSelectedCity=-1; //返回结果，先暂时把其设置为-1
-
-    //==============================================================================
-    //计算当前城市和没去过的城市之间的信息素总和
-
-    double dbTotal=0.0,tA,tB;
-    double prob[N_CITY_COUNT]; //保存各个城市被选中的概率
-
-    for (int i=0;i<N_CITY_COUNT;i++)
-    {
-        if (m_nAllowedCity[i] == 1) //城市没去过
-        {
-            //prob[i]=pow(d_Trial[m_nCurCityNo*N_CITY_COUNT+i],ALPHA)*pow(1.0/d_Distance[m_nCurCityNo*N_CITY_COUNT+i],BETA); //该城市和当前城市间的信息素
-        	tA=d_Trial[m_nCurCityNo*N_CITY_COUNT+i];
-        	tB=1.0/d_Distance[m_nCurCityNo*N_CITY_COUNT+i];
-        	prob[i]=(tA*tA)*(tB*tB*tB); //ALPHA=2.0,BETA=3.0
-        	dbTotal=dbTotal+prob[i]; //累加信息素，得到总和
-        }
-        else //如果城市去过了，则其被选中的概率值为0
-        {
-            prob[i]=0.0;
-        }
-    }
-
-    //==============================================================================
-    //进行轮盘选择
-    double dbTemp=0.0;
-    if (dbTotal > 0.0) //总的信息素值大于0
-    {
-        //取一个随机数devData
-    	dbTemp=dbTotal * devData[m_nMovedCityCount+antID*N_CITY_COUNT-1];
-
-        for (int i=0;i<N_CITY_COUNT;i++)
-        {
-            if (m_nAllowedCity[i] == 1) //城市没去过
-            {
-                dbTemp=dbTemp-prob[i]; //这个操作相当于转动轮盘，如果对轮盘选择不熟悉，仔细考虑一下
-                if (dbTemp < 0.0) //轮盘停止转动，记下城市编号，直接跳出循环
-                {
-                    nSelectedCity=i;
-                    break;
-                }
-            }
-        }
-    }
-
-    //==============================================================================
-    //如果城市间的信息素非常小 ( 小到比double能够表示的最小的数字还要小 )
-    //那么由于浮点运算的误差原因，上面计算的概率总和可能为0
-    //会出现经过上述操作，没有城市被选择出来
-    //出现这种情况，就把第一个没去过的城市作为返回结果
-
-    //题外话：刚开始看的时候，下面这段代码困惑了我很长时间，想不通为何要有这段代码，后来才搞清楚。
-    if (nSelectedCity == -1)
-    {
-        for (int i=0;i<N_CITY_COUNT;i++)
-        {
-            if (m_nAllowedCity[i] == 1) //城市没去过
-            {
-                nSelectedCity=i;
-                break;
-            }
-        }
-    }
-
-    //==============================================================================
-    //返回结果，就是城市的编号
-    return nSelectedCity;
-}
-
-//蚂蚁在城市间移动
-__device__
-void CAnt::antMove(int antID,double *d_Distance,double *d_Trial,float *devData)
-{
-    int nCityNo=antChooseNextCity(antID,m_nMovedCityCount,d_Distance,d_Trial,devData); //选择下一个城市
-
-    m_nPath[m_nMovedCityCount]=nCityNo; //保存蚂蚁走的路径
-    m_nAllowedCity[nCityNo]=0; //把这个城市设置成已经去过了
-    m_nCurCityNo=nCityNo; //改变当前所在城市为选择的城市
-    m_nMovedCityCount++; //已经去过的城市数量加1
-}
-
-//计算蚂蚁走过的路径长度
-__device__
-void CAnt::antCalPathLength(int antID,double *d_Distance)
-{
-    m_dbPathLength=0.0; //先把路径长度置0
-    int m=0;
-    int n=0;
-
-    for (int i=1;i<N_CITY_COUNT;i++)
-    {
-        m=m_nPath[i];
-        n=m_nPath[i-1];
-        m_dbPathLength=m_dbPathLength+d_Distance[m*N_CITY_COUNT+n];
-    }
-
-    //加上从最后城市返回出发城市的距离
-    n=m_nPath[0];
-    m_dbPathLength=m_dbPathLength+d_Distance[m*N_CITY_COUNT+n];
 }
 
 //蚂蚁进行搜索
