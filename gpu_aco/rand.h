40f73589db478eea91c27b190651918fe6b605b7
bolan2014 <1139217488@qq.com>
2015-10-25 22:02:17 +0800

    add tsp file name

--- gpu_aco/rand.h
+++ gpu_aco/rand.h
@@ -3,18 +3,21 @@
 //for parallel cuRand
 float *devData;
 
-const int N_ANT_COUNT=256;
-const int N_CITY_COUNT=51; 
+const int N_ANT_COUNT = 256;
+const int N_CITY_COUNT = 51; 
+
+//tsp file name
+const char *tspFile = "tsp/eil51.tsp";
 
 void dev_rnd(unsigned int nSeed)
 {
         curandGenerator_t gen;
 
-        cudaMalloc((void **)&devData,N_ANT_COUNT*N_CITY_COUNT*sizeof(float));
+        cudaMalloc((void **)&devData, N_ANT_COUNT*N_CITY_COUNT*sizeof(float));
 
-        curandCreateGenerator(&gen,CURAND_RNG_PSEUDO_DEFAULT);
-        curandSetPseudoRandomGeneratorSeed(gen,nSeed);
-        curandGenerateUniform(gen,devData,N_ANT_COUNT*N_CITY_COUNT);
+        curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
+        curandSetPseudoRandomGeneratorSeed(gen, nSeed);
+        curandGenerateUniform(gen, devData, N_ANT_COUNT*N_CITY_COUNT);
 
         curandDestroyGenerator(gen);
 }
