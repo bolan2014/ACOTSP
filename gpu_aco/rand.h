e204f4685e44dbf886161f60bfab6a9dc05a68bd
bolan2014 <1139217488@qq.com>
2015-11-02 22:43:35 +0800

    use tsp file of d493

--- gpu_aco/rand.h
+++ gpu_aco/rand.h
@@ -4,11 +4,11 @@
 float *devRnd;
 
 const int N_ANT_COUNT = 1024;
-const int N_CITY_COUNT = 318; 
+const int N_CITY_COUNT = 493; 
 const int N_IT_COUNT = 15;
 
 //tsp file name
-const char *tspFile = "tsp/lin318.tsp";
+const char *tspFile = "tsp/d493.tsp";
 
 void dev_rnd(unsigned int nSeed)
 {
